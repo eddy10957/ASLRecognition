5d5178f9766dec72c3ae616d63e3b2ac1a963c8c
eddy10957 <eddytd96@gmail.com>
2023-06-19 09:09:20 +0200

    First Commit

---
+++ ASLRecognition/ASLRecognition/ASLRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASLRecognitionApp.swift
+//  ASLRecognition
+//
+//  Created by Edoardo Troianiello on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ASLRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
